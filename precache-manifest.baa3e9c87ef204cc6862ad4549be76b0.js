cd0d8fb710084dd0c5021d08d43da71a236e619e
tulpia <teihotaata.alec@gmail.com>
2019-07-20 16:23:11 +0200

    Updates

---
+++ precache-manifest.baa3e9c87ef204cc6862ad4549be76b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eccc4227ae590b9b97e1af0da7801d01",
+    "url": "/react-tuto/index.html"
+  },
+  {
+    "revision": "bfcdeb5da15f740b5957",
+    "url": "/react-tuto/static/css/main.6395af23.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/react-tuto/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "bfcdeb5da15f740b5957",
+    "url": "/react-tuto/static/js/main.7a45741e.chunk.js"
+  },
+  {
+    "revision": "08e299a1aacc4d685c28",
+    "url": "/react-tuto/static/js/runtime~main.01f2d037.js"
+  },
+  {
+    "revision": "5197228f16e747337fa9ecf9ec0af01e",
+    "url": "/react-tuto/static/media/HelveticaNeueLTStd-Ex.5197228f.otf"
+  },
+  {
+    "revision": "c50cafe27126364ceb9f08dfd666645f",
+    "url": "/react-tuto/static/media/HelveticaNeueLTStd-Roman.c50cafe2.otf"
+  }
+]);
